b435e3f254d4b57059ace5054f15ff648adcce9a
joey krim <joeykrim@gmail.com>
2011-03-22 10:51:30 -0400

    Updated version to 2.9
    Signed-off-by: joey krim <joeykrim@gmail.com>

--- src/com/joeykrim/wimaxkeys/WimaxKeys.java
+++ src/com/joeykrim/wimaxkeys/WimaxKeys.java
@@ -39,7 +39,7 @@ public class WimaxKeys extends Activity {
 	private boolean disAccepted;
 	static final int DIALOG_DISCLAIMER_ID = 0;
 	static final int DIALOG_ABOUT_ID = 1;
-	static final double CURRENT_VERSION_ID = 2.8;
+	static final double CURRENT_VERSION_ID = 2.9;
 	private static String LOG_TAG = "WimaxKeyCheck";
 	private static String PHONE_EVO = "EVO";
 	private static String PHONE_SHIFT = "EVO Shift";
